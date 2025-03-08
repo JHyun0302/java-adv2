6bd6b8081615feac661553becbac369f0e7d515c
JHyun0302 <wogustkfkd11@gmail.com>
2025-03-08 10:59:29 +0900

    :sparkles: [Feat] 회원 관리 예제2 - 파일에 보관

---
+++ src/io/member/impl/FileMemberRepository.java
@@ -0,0 +1,49 @@
+package io.member.impl;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import io.member.Member;
+import io.member.MemberRepository;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileMemberRepository implements MemberRepository {
+
+    private static final String FILE_PATH = "temp/members-txt.dat";
+    private static final String DELIMITER = ",";
+
+    @Override
+    public void add(Member member) {
+        //true: 파일 끝에 이어서 write
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH, UTF_8, true))) {
+            bw.write(member.getId() + DELIMITER + member.getName() + DELIMITER + member.getAge());
+            bw.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } // try-with-resources : try문 벗어나는 순간 자동적으로 close() 호출
+    }
+
+    @Override
+    public List<Member> findAll() {
+        List<Member> members = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH, UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // id1,name1,30
+                String[] memberData = line.split(DELIMITER);
+                members.add(new Member(memberData[0], memberData[1], Integer.valueOf(memberData[2])));
+            }
+            return members;
+        } catch (FileNotFoundException e) {
+            return new ArrayList<>();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
