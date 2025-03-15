9e22af7a880808b46452020b0e41126eb6861ad9
JHyun0302 <wogustkfkd11@gmail.com>
2025-03-15 23:31:27 +0900

    :sparkles: [Feat] 네트워크 프로그램5 - 자원 정리2

---
+++ src/network/tcp/v5/ServerV5.java
@@ -0,0 +1,30 @@
+package network.tcp.v5;
+
+import static util.MyLogger.log;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * `main` 스레드는 새로운 연결이 있을 때 마다 `Session` 객체와 별도의 스레드를 생성하고, 별도의 스레드가 `Session` 객체를 실행
+ */
+public class ServerV5 {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("서버 시작");
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        log("서버 소켓 시작 - 리스닝 포트: " + PORT);
+
+        while (true) {
+            Socket socket = serverSocket.accept(); // 블로킹
+            log("소켓 연결: " + socket);
+
+            SessionV5 session = new SessionV5(socket);
+            Thread thread = new Thread(session);
+            thread.start();
+        }
+    }
+}
